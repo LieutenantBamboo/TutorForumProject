8bdaab9a2ae84e9eba98b00e3b26d4c0df80678c
Kyriacos Demetriou <junior8395@hotmail.com>
2017-02-26 15:04:49 +0000

    Set up the pages, added apropriate href to the base html to link to those pages

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,12 @@ from forum import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about')
+    url(r'^about/', views.about, name='about'),
+	url(r'^contact_us/', views.contact_us, name='contact_us'),
+	url(r'^categories/', views.categories, name='categories'),
+	url(r'^hall_of_shame/', views.hallOfShame, name='hallOfShame'),
+	url(r'^latest_news/', views.latestNews, name='latestNews'),
+	url(r'^login/', views.login, name='login'),
+	url(r'^FAQ/', views.FAQ, name='FAQ'),
+	url(r'^leisure/', views.leisure, name='leisure'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -12,6 +12,38 @@ def about(request):
     context_dict = {}
     return render(request, 'forum/about.html', context_dict)
 
-
 def home(request):
     return HttpResponseRedirect(reverse('index'))
+
+	
+def contact_us(request):
+    context_dict = {}
+    return render(request, 'forum/contact_us.html', context_dict)
+	
+def categories(request):
+    context_dict = {}
+    return render(request, 'forum/categories.html', context_dict)
+
+def hallOfShame(request):
+    context_dict = {}
+    return render(request, 'forum/hallOfShame.html', context_dict)
+
+def latestNews(request):
+    context_dict = {}
+    return render(request, 'forum/latestNews.html', context_dict)
+
+def login(request):
+    context_dict = {}
+    return render(request, 'forum/login.html', context_dict)
+
+def FAQ(request):
+    context_dict = {}
+    return render(request, 'forum/FAQ.html', context_dict)
+
+def leisure(request):
+    context_dict = {}
+    return render(request, 'forum/leisure.html', context_dict)
+
+
+
+
