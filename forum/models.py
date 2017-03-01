f7900a0c690ec46dddd26d477312d369730d318f
LieutenantBamboo <mattconnorday@gmail.com>
2017-03-01 13:24:45 +0000

    Added external CSS functionality and changed the base to look snazzier

--- forum/models.py
+++ forum/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True)
 
-	def __str__(self): # For Python 2, use __unicode__ too
-		return self.name
+    def __str__(self):  # For Python 2, use __unicode__ too
+        return self.name
 
 
-		
-		
-		
+class QuestionPage(models.Model):
+    pass
+
+
 class Question(models.Model):
-    category=models.ForeignKey(Category)
+    category = models.ForeignKey(Category)
     max_length = 128
     title = models.CharField(max_length=max_length, unique=True)
     views = models.IntegerField(default=0)
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
-    text_field=models.CharField(max_length=10000, unique=False)
+    text_field = models.CharField(max_length=10000, unique=False)
     slug = models.SlugField()
 
     def save(self, *args, **kwargs):
@@ -49,3 +52,19 @@ class Comment(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    # Required Line - links class to model instance
+    user = models.OneToOneField(User)
+
+    # Additonal Attributes:
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # __unicode__() override
+    def __str__(self):
+        return self.user.username
+
+    # Defined for compatability's sake
+    def __unicode__(self):
+        return self.user.username
--- forum/views.py
+++ forum/views.py
@@ -1,52 +1,60 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
-from forum.models import Category,Question
+from forum.models import Category, Question
 
 
 def index(request):
-	category_list = Category.objects.order_by('name')
-	context_dict = {'categories':category_list}
-	return render(request, 'forum/index.html', context_dict)
+    category_list = Category.objects.order_by('name')
+    context_dict = {'categories': category_list}
+    return render(request, 'forum/index.html', context_dict)
 
 
 def about(request):
     context_dict = {}
     return render(request, 'forum/about.html', context_dict)
 
+
+# Later to be repurposed if the site produces multiples apps
 def home(request):
     return HttpResponseRedirect(reverse('index'))
 
-	
+
 def contact_us(request):
     context_dict = {}
     return render(request, 'forum/contact_us.html', context_dict)
-	
+
+
 def categories(request):
     context_dict = {}
     return render(request, 'forum/categories.html', context_dict)
+
 
 def hallOfShame(request):
     context_dict = {}
     return render(request, 'forum/hallOfShame.html', context_dict)
 
+
 def latestNews(request):
     context_dict = {}
     return render(request, 'forum/latestNews.html', context_dict)
+
 
 def login(request):
     context_dict = {}
     return render(request, 'forum/login.html', context_dict)
 
+
 def FAQ(request):
     context_dict = {}
     return render(request, 'forum/FAQ.html', context_dict)
 
+
 def leisure(request):
     context_dict = {}
     return render(request, 'forum/leisure.html', context_dict)
-	
-#def show_category(request, category_name_slug):
+
+# def show_category(request, category_name_slug):
 #	context_dict={}
 #	try:
 #		category=Category.objects.get(slug=category_name_slug)
@@ -60,6 +68,3 @@ def leisure(request):
 #		context_dict['pages'] = None
 #	
 #	return render(request, 'forum/show_categories.html', context_dict)
-
-
-
