b9c64d312b5ab7f33ba13dd311c5380860b23613
Emily Youngs <2138946y@student.gla.ac.uk>
2017-03-21 17:43:32 +0000

    Bootstrap implemented and fixed question icon

--- TutorForumProject/urls.py
+++ TutorForumProject/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from forum import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.home, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^forum/', include('forum.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
