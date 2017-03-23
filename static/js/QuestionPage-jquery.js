75be906083611412a56ecc0c8808cf18e61e7668
LieutenantBamboo <mattconnorday@gmail.com>
2017-03-23 15:59:45 +0000

    Minor Fix

--- static/js/QuestionPage-jquery.js
+++ static/js/QuestionPage-jquery.js
@@ -1,14 +1,19 @@
+$('.vote').click(function () {
+        $(this).toggleClass('on');
+    });
+
 $(document).ready(function () {
+
     $('#up').on('click', function (event) {
         event.preventDefault();
         var element = $(this);
         $.ajax({
             url: '/like_questionPost',
             type: 'GET',
-            data: { questionPost: element.attr("data")},
+            data: {questionPost: element.attr("data")},
 
             success: function (response) {
-                    element.html(''+response);
+                element.html('' + response);
 
             }
         })
